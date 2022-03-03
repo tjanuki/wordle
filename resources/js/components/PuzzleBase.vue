b5d7cd5d0c0127a3c11867773ee90cbe2c91c676
tjanuki <t.januki@gmail.com>
2022-03-03 10:43:40 -0500

    Add Keyboard event listener

--- resources/js/components/PuzzleBase.vue
+++ resources/js/components/PuzzleBase.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex flex-col justify-between items-center h-screen w-full">
+  <div class="flex flex-col justify-between items-center h-screen w-full"
+       @keyup.delete="keyUp"
+  >
     <header class="flex justify-center py-2 border border-bottom w-full">
       <h1 class="text-4xl font-bold font-serif">Wordle</h1>
     </header>
@@ -16,5 +18,31 @@ import PuzzleKeyboards from "./PuzzleKeyboards";
 
 export default {
   components: {PuzzleKeyboards, PuzzleTable},
+  methods: {
+    keyUp(event) {
+      // console.log('Base', event);
+      // console.log('KeyCode:' + event.key, event.keyCode);
+      if (event.keyCode === 8) { // Backspace or Delete
+        this.$store.dispatch('deleteWord')
+      }
+      if (event.keyCode === 13) { // Enter
+        this.$store.dispatch('enterRow')
+      }
+      if (65 <= event.keyCode && event.keyCode <= 90) { // A - Z
+        // console.log('KeyCode:' + event.key, event.keyCode);
+        // console.log('FromCharCode:' + String.fromCharCode(event.keyCode));
+        this.$store.dispatch('clickButton', {
+          key: String.fromCharCode(event.keyCode)
+        })
+      }
+    }
+  },
+  created() {
+    document.addEventListener('keyup', this.keyUp)
+  },
+  onUnmounted() {
+    document.removeEventListener('keyup')
+  }
+
 }
 </script>
